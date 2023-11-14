bc5ad12a4f1b3c45a75f33ca8d7a84a11dba631e
Seundede <omadoyedede0@gmail.com>
2023-11-14 21:53:46 +0100

    feat:navigation bar change

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,70 +1,71 @@
-import React, { useState } from "react";
+import React from "react";
+import { Disclosure } from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import { Link } from "react-router-dom";
-import { FiMenu } from "react-icons/fi";
-import { AiOutlineClose } from "react-icons/ai";
-import { DarkModeToggle } from "tailwind-darkmode-toggle";
 
-const Navbar = () => {
-  const data = [
-    {
-      title: "Skills",
-      link: "/skills",
-    },
-    {
-      title: "Projects",
-      link: "/projects",
-    },
-  ];
-
-  const [nav, setNav] = useState(false);
-
-  const toggle = () => {
-    setNav(!nav);
-  };
-
+export default function Navbar() {
   return (
-    <div>
-      <nav className="w-full h-[60px] md:h-[80px] px-8 xl:px-16 flex justify-between items-center dark:bg-black dark:text-white">
-        <Link to="/" className="text-xl lg:text-3xl cursor-pointer">
-          DEDE
-        </Link>
-
-        <ul className="hidden md:flex items-center">
-          {data.map((item, index) => (
-            <Link to={item.link} key={index}>
-              <li className="px-4 cursor-pointer lg:text-xl hover:underline">
-                {item.title}
-              </li>
-            </Link>
-          ))}
-          <DarkModeToggle className="w-10 h-10 text-gray-800 dark:text-gray-300" />
-        </ul>
-
-        {/* Hamburger */}
-        <div className=" block md:hidden" onClick={toggle}>
-          {nav ? <AiOutlineClose size={28} /> : <FiMenu size={28} />}
-        </div>
-        {/* mobile menu */}
-        <div
-          className={
-            nav
-              ? "md:hidden w-full absolute top-[70px] left-0 p-2 flex justify-center  z-40  h-screen bg-gray-100 dark:bg-black dark:text-white"
-              : "absolute left-[-100%]"
-          }
-        >
-          <ul>
-            {data.map((item, index) => (
-              <Link to={item.link} key={index}>
-                <li className="py-[30px] text-lg">{item.title}</li>
+    <Disclosure as="nav" className="bg-white shadow">
+      {({ open }) => (
+        <>
+          <div className=" px-4 sm:px-6 lg:px-8">
+            <div className="flex h-16 justify-between">
+              <Link
+                to="/"
+                className="text-xl lg:text-3xl cursor-pointer flex items-center"
+              >
+                DEDE
               </Link>
-            ))}
-            <DarkModeToggle className="w-10 h-10 text-gray-800 dark:text-gray-300" />
-          </ul>
-        </div>
-      </nav>
-      <hr className="border border-gray-100 my-2 dark:border-white" />
-    </div>
-  );
-};
 
-export default Navbar;
+              <div className="hidden sm:ml-6 sm:flex sm:items-center gap-8">
+                <Link
+                  to="/skills"
+                  className="inline-flex items-center hover:border-b-2 hover:border-black px-1 pt-1 text-sm font-medium text-gray-900 cursor pointer"
+                >
+                  Skills
+                </Link>
+                <Link
+                  to="/projects"
+                  className="inline-flex items-center hover:border-b-2 hover:border-black px-1 pt-1 text-sm font-medium text-gray-900"
+                >
+                  Projects
+                </Link>
+              </div>
+              <div className="-mr-2 flex items-center sm:hidden">
+                {/* Mobile menu button */}
+                <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-black">
+                  <span className="absolute -inset-0.5" />
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                  ) : (
+                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                  )}
+                </Disclosure.Button>
+              </div>
+            </div>
+          </div>
+
+          <Disclosure.Panel className="sm:hidden">
+            <div className="space-y-4 pb-3 pt-2">
+              <Disclosure.Button
+                as="a"
+                href="/skills"
+                className="block hover:border-l-4 hover:border-indigo-500  py-2 pl-3 pr-4 text-base font-medium text-gray-900 cursor-pointer"
+              >
+                Skills
+              </Disclosure.Button>
+              <Disclosure.Button
+                as="a"
+                href="/projects"
+                className="block hover:border-l-4 hover:border-black  py-2 pl-3 pr-4 text-base font-medium text-gray-900 cursor-pointer"
+              >
+                Projects
+              </Disclosure.Button>
+            </div>
+          </Disclosure.Panel>
+        </>
+      )}
+    </Disclosure>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,19 +1,15 @@
-import React from 'react';
-import './index.css';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
-import { ThemeProvider } from 'tailwind-darkmode-toggle';
-
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
   <React.StrictMode>
-    <ThemeProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ThemeProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
